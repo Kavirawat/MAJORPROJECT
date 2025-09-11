c8cc0241c5c5b66b3ffd3cd04a7b38c0f1df6210
kavi <kairrawat896@gmail.com>
2025-09-11 13:03:53 +0530

    add some files

--- classroom/server.js
+++ classroom/server.js
@@ -5,6 +5,22 @@ app.get("/", (req, res) => {
   res.send("This is root route");
 });
 
+//Index - Users
+app.get("/users", (req, res) => {
+  res.send("Get for users");
+});
+
+//show - Users
+app.get("/users/:id", (req, res) => {
+  res.send("Get for show users");
+});
+
+//show - Users
+app.post("/users", (req, res) => {
+  res.send("post for  for show users");
+});
+
+
 app.listen(3000, () => {
   console.log("Server is running on 3000");
 });
